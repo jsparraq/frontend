1f63ee598ee0a609d12cb37e2b17d9c0e1c6bc6e
Sebastian Parra <sebastian.parra96@gmail.com>
2021-12-16 22:27:48 -0500

    Add graphQL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { Switch, Route } from "react-router-dom";
-import { ApolloProvider  } from "apollo-boost";
 
-import client from "./graphQL/instance"
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import PageOne from "./pages/PageOne";
@@ -11,27 +9,25 @@ import Login from "./pages/Login";
 
 function App() {
     return (
-        <ApolloProvider client={client}>
-            <Layout>
-                <Switch>
-                    <Route path="/" exact>
-                        <Home />
-                    </Route>
-                    <Route path="/page-one">
-                        <PageOne />
-                    </Route>
-                    <Route path="/page-two">
-                        <PageTwo />
-                    </Route>
-                    <Route path="/page-three">
-                        <PageThree />
-                    </Route>
-                    <Route path="/login">
-                        <Login/>
-                    </Route>
-                </Switch>
-            </Layout>
-        </ApolloProvider>
+        <Layout>
+            <Switch>
+                <Route path="/" exact>
+                    <Home />
+                </Route>
+                <Route path="/page-one">
+                    <PageOne />
+                </Route>
+                <Route path="/page-two">
+                    <PageTwo />
+                </Route>
+                <Route path="/page-three">
+                    <PageThree />
+                </Route>
+                <Route path="/login">
+                    <Login/>
+                </Route>
+            </Switch>
+        </Layout>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from "react-dom";
 import "./styles/index.scss";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ApolloClient, ApolloProvider, InMemoryCache, HttpLink } from '@apollo/client';
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'http://localhost:3002/graphql'
+    })
+})
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
+    <ApolloProvider client={client}>
+        <Router>
+            <App />
+        </Router>
+    </ApolloProvider>,
     document.getElementById("root")
 );
